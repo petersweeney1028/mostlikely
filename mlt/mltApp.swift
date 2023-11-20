a028d58b90e00a297b4b28d9f29df97d55003cf5
peter sweeney <petersweeney1028@gmail.com>
2023-11-20 11:30:49 -0500

    Initial Commit

---
+++ mlt/mltApp.swift
@@ -0,0 +1,17 @@
+//
+//  mltApp.swift
+//  mlt
+//
+//  Created by Peter Sweeney on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct mltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
